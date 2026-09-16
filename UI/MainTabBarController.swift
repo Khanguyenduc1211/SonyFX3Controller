8c68fd7094f131c9b4a4fbfd133e1bc0ffaff8ee
khanguyenduc <137383606+Khanguyenduc1211@users.noreply.github.com>
2026-09-17 03:12:33 +0700

    Add Live View tab

--- UI/MainTabBarController.swift
+++ UI/MainTabBarController.swift
@@ -2,13 +2,37 @@ import UIKit
 
 final class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
-        super.viewDidLoad(); view.backgroundColor = Theme.background; tabBar.barStyle = .black; tabBar.tintColor = Theme.blue
+        super.viewDidLoad()
+        view.backgroundColor = Theme.background
+        tabBar.barStyle = .black
+        tabBar.tintColor = Theme.blue
+
+        let live = UINavigationController(rootViewController: LiveViewViewController())
         let control = UINavigationController(rootViewController: ControlViewController())
         let focus = UINavigationController(rootViewController: FocusViewController())
         let format = UINavigationController(rootViewController: RecordFormatViewController())
-        control.tabBarItem = UITabBarItem(title: "CONTROL", image: UIImage(systemName: "dot.radiowaves.left.and.right"), tag: 0)
-        focus.tabBarItem = UITabBarItem(title: "FOCUS", image: UIImage(systemName: "viewfinder"), tag: 1)
-        format.tabBarItem = UITabBarItem(title: "FORMAT", image: UIImage(systemName: "film"), tag: 2)
-        viewControllers = [control, focus, format]
+
+        live.tabBarItem = UITabBarItem(
+            title: "LIVE",
+            image: UIImage(systemName: "video"),
+            tag: 0
+        )
+        control.tabBarItem = UITabBarItem(
+            title: "CONTROL",
+            image: UIImage(systemName: "dot.radiowaves.left.and.right"),
+            tag: 1
+        )
+        focus.tabBarItem = UITabBarItem(
+            title: "FOCUS",
+            image: UIImage(systemName: "viewfinder"),
+            tag: 2
+        )
+        format.tabBarItem = UITabBarItem(
+            title: "FORMAT",
+            image: UIImage(systemName: "film"),
+            tag: 3
+        )
+
+        viewControllers = [live, control, focus, format]
     }
 }
